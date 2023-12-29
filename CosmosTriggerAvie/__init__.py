5e018b4f04409f4aaa20f9dbd092a2b3f3b46ed9
Sefdine Nassuf <74672023+Sefdine@users.noreply.github.com>
2023-12-29 12:18:22 +0100

    update functions: sending request to logic app

--- CosmosTriggerAvie/__init__.py
+++ CosmosTriggerAvie/__init__.py
@@ -7,26 +7,16 @@ from azure.core.credentials import AzureKeyCredential
 from azure.ai.textanalytics import TextAnalyticsClient, TextDocumentInput
 from azure.cosmos import CosmosClient
 
-# CosmosDB configuration
-SETTINGS = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://mentallboys.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'bE4xRSJNgkVGjDiMLv0o0svZ9DzQ188fgv1wuYhNSwKVfyGTFR1hQY9hlkV1BqnnvfGtZqddWpiTACDbvxZlog=='),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'Reviews'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'ItemsUpdated'),
-}
+# Send a HTTP request to Logic App
+def send_http_request_logic_app(data):
+    url = "https://prod-03.francecentral.logic.azure.com:443/workflows/33f0cc89aebe480698102e117d8d873b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=KKsFooE1O7AiJ0mMaoB5Le5cfyuLASyRC5fsIdNZZSs"
 
-# Initialize Cosmos client
-client = CosmosClient(SETTINGS['host'], credential=SETTINGS['master_key'])
-database = client.get_database_client(SETTINGS['database_id'])
-container = database.get_container_client(SETTINGS['container_id'])
-
-# Function to insert data into Cosmos DB
-def insert_into_cosmos(data):
     try:
-        container.create_item(body=data)
-        logging.info("Inserted data into Cosmos DB.")
+        response = requests.post(url=url, json=data)
+        logging.info(f"Request sent to Logic App")
     except Exception as e:
-        logging.error(f"Failed to insert data into Cosmos DB: {str(e)}")
+        logging.error(f"Failed to send request to logic app {str(e)}")
+
 
 # functions to send message into slack
 def send_message_to_slack(message):
@@ -38,7 +28,7 @@ def send_message_to_slack(message):
         "text": message
     }
 
-    url = "https://hooks.slack.com/services/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"
+    url = "https://hooks.slack.com/services/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"
 
     try:
         response = requests.post(url, headers=headers, json=data)
@@ -49,8 +39,8 @@ def send_message_to_slack(message):
 # Function to analyze sentiment 
 def analyse_sentiment(review_text):
     # Connect to Azure Language Analysis service
-    key = os.environ.get('TEXT_ANALYTICS_KEY', '83f7f2fbcace4636b37c488c05cc9570')
-    endpoint = os.environ.get('TEXT_ANALYTICS_ENDPOINT', 'https://mentallboys.cognitiveservices.azure.com/')
+    key = os.environ.get('TEXT_ANALYTICS_KEY', 'd51e7a4cdbf04610bd9218cd0faf5a87')
+    endpoint = os.environ.get('TEXT_ANALYTICS_ENDPOINT', 'https://mentalboysreviews.cognitiveservices.azure.com/')
     credentials = AzureKeyCredential(key)
     client = TextAnalyticsClient(endpoint=endpoint, credential=credentials)
 
@@ -83,7 +73,6 @@ def main(documents: func.DocumentList) -> str:
 
         item['sentiment'] = sentiment
 
-        # Update the data in cosmos db
-        logging.info(f"Item:  {item}")
-        insert_into_cosmos(item)
+        # Send request to logic app
+        send_http_request_logic_app(item)
         logging.info(f"Review texts : {review_text}, \t Score : {sentiment}")
